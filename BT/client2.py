0320b9a4b14f93840e57d14509db2b51cd409c4c
MikszaM <miksza.michal@gmail.com>
2018-05-29 11:15:22 -0700

    bl

--- BT/client2.py
+++ BT/client2.py
@@ -1,17 +1,15 @@
-"""
-A simple Python script to send messages to a sever over Bluetooth
-using PyBluez (with Python 2).
-"""
+from bluetooth import *
 
-import bluetooth
+# Create the client socket
+client_socket=BluetoothSocket( RFCOMM )
 
-serverMACAddress = 'B8:27:EB:3A:15:72'
-port = 3
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((serverMACAddress, port))
-while 1:
-    text = raw_input() # Note change to the old (Python 2) raw_input
-    if text == "quit":
-    break
-    s.send(text)
-sock.close()
+client_socket.connect(("B8:27:EB:3A:15:72", 3))
+
+client_socket.send("Hello World")
+
+print "Finished"
+
+client_socket.close()
+
+
+
--- BT/server2.py
+++ BT/server2.py
@@ -1,20 +1,16 @@
-import bluetooth
+from bluetooth import *
 
-hostMACAddress = 'B8:27:EB:3A:15:72' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-port = 3
-backlog = 1
-size = 1024
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.bind((hostMACAddress, port))
-s.listen(backlog)
-try:
-    client, clientInfo = s.accept()
-    while 1:
-        data = client.recv(size)
-        if data:
-            print(data)
-            client.send(data) # Echo back to client
-except:	
-    print("Closing socket")
-    client.close()
-    s.close()
+server_socket=BluetoothSocket( RFCOMM )
+
+server_socket.bind(("", 3 ))
+server_socket.listen(10)
+
+client_socket, address = server_socket.accept()
+
+data = client_socket.recv(1024)
+
+print "received [%s]" % data
+
+client_socket.close()
+server_socket.close()
+
